c7e87d19b7a60935be47344ae7c28c72ed3461de
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-01-23 16:00:44 +0530

    working

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Lucy/Conway's Game of Life
-# Build directory: E:/Lucy/Conway's Game of Life/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
